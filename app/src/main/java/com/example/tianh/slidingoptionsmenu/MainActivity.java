2e555ec073c06f1de48c78b3e1d630fb418f9b20
tianhonglong <tianhonglong@yeah.net>
2019-09-25 17:43:15 +0800

    no message

--- app/src/main/java/com/example/tianh/slidingoptionsmenu/MainActivity.java
+++ app/src/main/java/com/example/tianh/slidingoptionsmenu/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.tianh.slidingoptionsmenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.menu.mylibrary.SlidingOptionsMenu;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SlidingOptionsMenu slidingOptionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        slidingOptionsMenu = findViewById(R.id.slidingOptionsMenu);
+        slidingOptionsMenu.setCurrentItem(3);
     }
 }
